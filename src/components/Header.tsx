235423250886fd9d8db318da3752f7fd553c7580
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2025-08-11 06:20:10 +0000

    'Inshoyim' yozuvi o'zi tursin favicon yonida qolganalri kerakamas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ function MobileNav({ user }: { user: User | null }) {
         <SheetHeader>
           <SheetTitle asChild>
              <Link href="/" className="flex items-center gap-2" onClick={() => setIsOpen(false)}>
-                <Feather className="h-6 w-6 text-primary" />
+                
                 <span className="font-headline text-xl font-bold">Inshoyim</span>
             </Link>
           </SheetTitle>
@@ -149,13 +149,13 @@ export function Header() {
                   <MobileNav user={user} />
               </div>
               <Link href="/" className="hidden md:flex items-center gap-2">
-              <Feather className="h-6 w-6 text-primary" />
+              
               <span className="font-headline text-xl font-bold">Inshoyim</span>
               </Link>
           </div>
 
           <Link href="/" className="flex md:hidden items-center gap-2">
-            <Feather className="h-6 w-6 text-primary" />
+            
             <span className="font-headline text-xl font-bold">Inshoyim</span>
           </Link>
           
